7fc12699fe3295375675cd85b82449c4e9b7622e
Ortega02 <io.ortega.tony@gmail.com>
2024-04-09 19:51:20 -0600

    Crear menu

--- app/src/main/java/com/example/desafio2dsm/MainActivity.java
+++ app/src/main/java/com/example/desafio2dsm/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.desafio2dsm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMenu=findViewById(R.id.btnMenu);
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SplashScreen.class));
+            }
+        });
     }
 }
